b2d0dfa2f222677dc1f7ed5341e56dfc6f659d20
amangoyal097 <amangoyal097@gmail.com>
2022-11-15 02:12:11 +0530

    Added plots for the river

---
+++ README.md
@@ -0,0 +1,10 @@
+# DSS Tool to calculated Environmental Flows and Water Quality Index
+
+The tool calculates environmental flow using the class and the release data given as input by the user and also supports calculating the water quality index from a csv output of Qual2k model.
+
+## Steps to run
+
+```
+pip install -r requirements.txt
+streamlit run main.py
+```
--- pages/Qual2k.py
+++ pages/Qual2k.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+from plotly.offline import iplot
+import plotly.express as px
 
 st.title("Qual2k Model")
 
@@ -26,15 +29,102 @@ if uploaded_file is not None and st.session_state.uploaded:
     arr = df.to_numpy()
     ans = []
     y = []
+    c = []
+    c1 = c2 = c3 = c4 = c5 = 0
+    k = []
     for i in arr:
         # print(0.17*i[0] + 0.12*i[1] + 0.1*(i[2] + i[3] + i[4]) + 0.08*i[5])
-        ans.append(
-            (0.17 * i[0] + 0.12 * i[1] + 0.1 * (i[2] + i[3] + i[4]) + 0.08 * i[5]) / 8.5
-        )
+        wqi = (
+            0.17 * i[0] + 0.12 * i[1] + 0.1 * (i[2] + i[3] + i[4]) + 0.08 * i[5]
+        ) / 8.5
+        ans.append(wqi)
         y.append(i[6])
-    fig, ax = plt.subplots()
-    ax.plot(y, ans)
-    ax.set_title("Water Quality Index vs Distance")
-    ax.set_xlabel("Distance (km)")
-    ax.set_ylabel("WQI")
-    st.pyplot(fig)
+        a = wqi
+        if a < 20:
+            c.append(1)
+            c1 = c1 + 1
+            k.append("Very Bad Quality")
+        elif a >= 20 and a < 40:
+            c.append(2)
+            c2 = c2 + 1
+            k.append("Bad Quality")
+        elif a >= 40 and a < 60:
+            c.append(3)
+            c3 = c3 + 1
+            k.append("Dubious quality")
+        elif a >= 60 and a < 80:
+            c.append(4)
+            c4 = c4 + 1
+            k.append("Satisfactory Quality")
+        else:
+            c.append(5)
+            c5 = c5 + 1
+            k.append("Good quality")
+
+    xs = y
+    ys = ans
+    df = pd.DataFrame({"x": xs, "y": ys, "z": c, "Class distribution": k})
+    theshhold = 20
+
+    fig = go.Figure()
+
+    fig.add_scattergl(
+        x=xs, y=df.y.where(df.z == 1), line={"color": "red"}, name="Very Bad Quality"
+    )
+    fig.add_scattergl(
+        x=xs, y=df.y.where(df.z == 2), line={"color": "orange"}, name="Bad Quality"
+    )
+    fig.add_scattergl(
+        x=xs, y=df.y.where(df.z == 3), line={"color": "yellow"}, name="Dubious quality"
+    )
+    fig.add_scattergl(
+        x=xs,
+        y=df.y.where(df.z == 4),
+        line={"color": "green"},
+        name="Satisfactory Quality",
+    )
+    fig.add_scattergl(
+        x=xs,
+        y=df.y.where(df.z == 5),
+        line={"color": "blue"},
+        name="Good quality",
+    )
+    fig.update_layout(
+        title="WQI vs Distance",
+        width=1000,
+        height=500,
+        font=dict(size=18),
+    )
+    st.plotly_chart(fig)
+
+    # fig = px.histogram(df['Class distribution'])
+
+    # fig.show()
+    an = [1, 2, 3, 4, 5]
+    bn = []
+    bn.append(c1)
+    bn.append(c2)
+    bn.append(c3)
+    bn.append(c4)
+    bn.append(c5)
+    colors = [
+        "Very Bad Quality",
+        "Bad Quality",
+        "Dubious quality",
+        "Satisfactory Quality",
+        "Good quality",
+    ]
+    fig = px.bar(
+        df,
+        x=an,
+        y=bn,
+        color=colors,
+        text=bn,
+        title="Data points per category of water quality",
+    )
+    fig.update_layout(
+        width=1000,
+        height=500,
+        font=dict(size=18),
+    )
+    st.plotly_chart(fig)
